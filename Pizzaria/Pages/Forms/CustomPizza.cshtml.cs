b2e6548a2946c6109922706e9bdf6b87848778ff
waridhasan195 <waridhasan195@gmail.com>
2024-09-16 22:56:07 +0600

    FirstPush

--- Pizzaria/Pages/Forms/CustomPizza.cshtml.cs
+++ Pizzaria/Pages/Forms/CustomPizza.cshtml.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Pizzaria.Models;
 
 namespace Pizzaria.Pages.Forms
 {
     public class CustomPizzaModel : PageModel
     {
+        [BindProperty]
+        public PizzasModel Pizza { get; set; }
         public void OnGet()
         {
         }
