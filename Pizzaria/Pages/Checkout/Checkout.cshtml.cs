24c905003c02ae5bd08e1fbf8179412768213272
waridhasan195 <waridhasan195@gmail.com>
2024-09-17 11:22:36 +0600

    Custom Pizza Complete

--- Pizzaria/Pages/Checkout/Checkout.cshtml.cs
+++ Pizzaria/Pages/Checkout/Checkout.cshtml.cs
@@ -3,10 +3,26 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Pizzaria.Pages.Checkout
 {
+    [BindProperties(SupportsGet =true)]
     public class CheckoutModel : PageModel
     {
+
+        public string PizzaName { get; set; }
+        public float PizzaPrice { get; set; }
+        public string ImageTitle { get; set; }
+
+
         public void OnGet()
         {
+            if (string.IsNullOrWhiteSpace(PizzaName))
+            {
+                PizzaName = "CheesePizza";
+            }
+            if (string.IsNullOrWhiteSpace(ImageTitle))
+            {
+                ImageTitle = "4";
+            }
         }
+
     }
 }
--- Pizzaria/Pages/Forms/CustomPizza.cshtml.cs
+++ Pizzaria/Pages/Forms/CustomPizza.cshtml.cs
@@ -8,8 +8,27 @@ namespace Pizzaria.Pages.Forms
     {
         [BindProperty]
         public PizzasModel Pizza { get; set; }
+        public float PizzaPrice {  get; set; }
         public void OnGet()
         {
         }
+
+        public IActionResult OnPost()
+        {
+            PizzaPrice = Pizza.BasePrice;
+
+            if (Pizza.TomatoSouce) PizzaPrice += 1;
+            if (Pizza.Cheese) PizzaPrice += 1;
+            if (Pizza.Peperoni) PizzaPrice += 1;
+            if (Pizza.Mushroom) PizzaPrice += 1;
+            if (Pizza.Tuna) PizzaPrice += 1;
+            if (Pizza.PineApple) PizzaPrice += 10;
+            if (Pizza.Ham) PizzaPrice += 1;
+            if (Pizza.Beef) PizzaPrice += 1;
+
+            return RedirectToPage("/Checkout/Checkout", new { Pizza.PizzaName, PizzaPrice });
+        }
     }
 }
+
+
